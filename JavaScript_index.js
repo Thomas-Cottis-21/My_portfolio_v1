b9062f29437cdb9d18227086a680afd0db5c9aa6
Thomas Joseph Cottis <tomcottis21@gmail.com>
2021-11-14 15:30:40 -0700

    created js page to index.html and included css. new resume

---
+++ JavaScript_index.js
@@ -0,0 +1,10 @@
+function popUp(){
+    let text;
+    let person = prompt("Please enter your name:", "Buzz Lightyear");
+    if (person == null || person == "") {
+        text = "User cancelled the prompt";
+    } else {
+        text = "Hello " + person + "! Thank you for your input and please contact me soon!";
+    }
+    document.getElementById("newPage").innerHTML = text;
+}
