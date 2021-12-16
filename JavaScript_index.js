609b4694dca6ce519073d38aee26600300336fac
Thomas Joseph Cottis <tomcottis21@gmail.com>
2021-12-15 23:53:35 -0700

    added jquery in about, next is dark mode. added animations to load on all pages, going to change to onscroll

--- JavaScript_index.js
+++ JavaScript_index.js
@@ -12,16 +12,16 @@ function popUp(){
     if (person == null || person == "") {
         text = "Thank you for your visit!";
 
-    }else if(h_morning > d.getHours() && d.getHours() < h_afternoon){
+    }else if(h_morning >= d.getHours() && d.getHours() < h_afternoon){
         //if 12 is greater than current time AND less than 17.
         text = `Good morning, ${person}! Thank you for taking time out of your morning.`;
 
-    }else if(h_afternoon > d.getHours() && d.getHours() > h_morning){
+    }else if(h_afternoon >= d.getHours() && d.getHours() > h_morning){
         //if 17 is greater than current time AND greater than 12.
         text = "Good afternoon, " + person + "! Thank you for taking time out of your day.";
 
     }else if(h_evening >= d.getHours() && d.getHours() > h_afternoon){
-        //if the current time is greater than or equal to 23.
+        //if the current time is less than or equal to 23 and greater than 17.
         text = "Good evening, " + person + "! Thank you for taking time out of your night."
     }
     document.getElementById("newPage").innerHTML = text;
@@ -31,5 +31,3 @@ function switchTheme() {
     var element = document.body;
     element.classList.toggle("switch-theme");
 }
-
-
