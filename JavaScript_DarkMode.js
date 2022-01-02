5baa59f887c19d34e5cae405ba4027e138919b32
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-01-01 19:32:29 -0700

    Dark Mode works, trying to chang icon onclick

--- JavaScript_DarkMode.js
+++ JavaScript_DarkMode.js
@@ -1,13 +1,22 @@
 let darkMode = localStorage.getItem("darkMode");
-const switchTheme = document.getElementById("darkness");
-
+const switchTheme = document.getElementById("dark");
 const enableDarkMode = () => {
     document.body.classList.add("switch-theme");
+    document.getElementById("firstH3").classList.add("switchH3");
+    document.getElementById("newPage").classList.add("switch-text");
+    document.getElementById("menu-dwnl").classList.add("switch-menu-button");
+    document.getElementById("menu-cont").classList.add("switch-menu-button");
+    document.getElementById("menu-home").classList.add("switch-menu-button");
     localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
     document.body.classList.remove("switch-theme");
+    document.getElementById("firstH3").classList.remove("switchH3");
+    document.getElementById("newPage").classList.remove("switch-text");
+    document.getElementById("menu-dwnl").classList.remove("switch-menu-button");
+    document.getElementById("menu-cont").classList.remove("switch-menu-button");
+    document.getElementById("menu-home").classList.remove("switch-menu-button");
     localStorage.setItem("darkMode", "disabled");
 };
 
@@ -17,6 +26,7 @@ if (darkMode === "enabled") {
 
 switchTheme.addEventListener("click", () => {
     darkMode = localStorage.getItem("darkMode");
+    
     if (darkMode !== "enabled") {
         enableDarkMode();
     }else{
