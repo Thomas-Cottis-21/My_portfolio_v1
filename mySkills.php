32800ae432dc052aa133478f30ba1ee936a38098
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-02-22 22:26:25 -0700

    task 3 completed

--- mySkills.php
+++ mySkills.php
@@ -2,15 +2,25 @@
 
 $mySkills = array("HTML", "CSS", "JavaScript", "PHP / MySQL");
 
+$myHobbies = array("Welding", "Woodworking", "Designing and creating modern furniture", "Creating aestheticaly pleasing things", "Building computers", "Playing videogames", "Travelling");
+
+function newList($array){
+    foreach ($array as $listItem){
+        echo "<ul>";
+    
+        echo $listItem;
+    
+        echo "</ul>";
+    };
+}
+
 echo "Although I have soft skills, my hard skills are: ";
 
-foreach ($mySkills as $skill){
-    echo "<ul>";
+newList($mySkills);
 
-    echo $skill;
+echo "Some of my hobbies are: ";
 
-    echo "</ul>";
-};
+newList($myHobbies);
 
 
 
