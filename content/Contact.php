9bf2b2f1dff5bd828d74f530b3ac953caa5f884d
Thomas Joseph Cottis <tomcottis21@gmail.com>
2022-03-09 23:11:16 -0700

    created new php for contact section in order to be more secure

--- content/Contact.php
+++ content/Contact.php
@@ -20,10 +20,28 @@
                     </nav>
                 </div>
             </header>
+            <?php
+                $name = $email = $number = $contact = $comments = "";
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $name = data_input($_POST["name"]);
+                    $email = data_input($_POST["email"]);
+                    $number = data_input($_POST["number"]);
+                    $contact = data_input($_POST["contact"]);
+                    $comments = data_input($_POST["comments"]);
+                }
+
+                function data_input($data) {
+                    $data = trim($data);
+                    $data = stripslashes($data);
+                    $data = htmlspecialchars($data);
+                    return $data;
+                }
+            ?>
     
             <h1> Let's make this happen</h1>
             
-            <form action="/php/thanks.php" method="post">
+            <form action= "<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
                 <label for="name">Name:</label>
                 <input type="text" name="name" id="name" placeholder="Johana Ramirez">
@@ -58,7 +76,41 @@
             
                 
             </form>
+                <?php
+                    if (isset($_POST["email"])){
+                        echo "<h3>Thank you so much!</h3>";
+                        echo "<h4>Your confirmation email should be on it's way! If not found immediately, please check your spam folder.</h4>";
+                        echo "<h4>You simply don't understand what your input means to me, thank you</h4>";
+                        echo "<h4>Your contact information was recorded as:</h4>";
+                        echo "<ul>
+                        <li>$name</li>
+                        <li>$email</li>
+                        <li>$number</li>
+                        <li>$contact</li>
+                        </ul>";
+                    }
+                ?>
+                <?php
+                    $to = "thomascottis@thomasandco.xyz";
+                    $subject ="New Client!";
 
+                    $message = $_POST["name"] . "\n" . $_POST["number"] . "\n" . $_POST["email"] . "\n" . $_POST["comments"];
+
+                    $message = wordwrap($message, 70);
+
+                    $headers = "From: thomascottis@thomasandco.xyz";
+
+                    mail($to, $subject, $message, $headers);
+
+                    $to = $_POST["email"];
+                    $subject = "Thank you!";
+
+                    $message = "Thank you " . $_POST["name"] . ", for having interest in me and my persuits. I will get back to you within 24 hours! I hope you have a wonderful rest of your day!";
+
+                    $headers2 = "From: thomascottis@thomasandco.xyz";
+
+                    mail($to, $subject, $message, $headers);
+                ?>
             <h3>Or contact me directly...</h3>
             <h2>
                 Cellphone: <a class="link" href="tel:3853352336">(385)-335-2336</a></br>
